eec38e0fbe5b5bdbcb5fd538917d6b547e751289
mythosmystery <axiysfire@gmail.com>
2021-06-13 03:19:32 -0500

    updated bspwm, polybar, added package list

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,8 @@ DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
+killall -q volumeicon
+killall -q cbatticon
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
