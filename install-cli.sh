1425678ce8067f52d81aa4f2f959b94b1349391d
Hunter Barton <axiysfire@gmail.com>
2022-02-02 01:31:57 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -14,7 +14,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
-fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt -y update
