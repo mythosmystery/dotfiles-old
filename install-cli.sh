41b10ff9a7ce8dc407eeda9bd6be7ffd24ccbcf7
Hunter Barton <axiysfire@gmail.com>
2022-02-01 20:35:44 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -12,8 +12,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+sudo apt -y update
+sudo apt -y install gh
 gh auth login
 gh repo clone dotfiles ~/.cfg -- --bare
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
