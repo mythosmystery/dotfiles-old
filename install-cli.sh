0b1599bc0cbcc82270b3668fab0d446af361479f
Hunter Barton <axiysfire@gmail.com>
2022-02-02 15:21:27 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -1,10 +1,19 @@
+echo "adding fish repo and updating all software"
 sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo apt -y update
 sudo apt -y upgrade
+
+echo "install cli apps"
 sudo apt -y install zsh tmux gcc neovim vifm fish neofetch git
+
+echo "install rust"
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . "$HOME/.cargo/env"
+
+echo "install exa"
 cargo install exa
+
+echo "install zsh plugins"
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -14,11 +23,19 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
+
+echo "install github cli"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt -y update
 sudo apt -y install gh
-bash -c "gh auth login"
+
+echo "login to github"
+gh auth login
+
+echo "clone dotfiles repo"
 gh repo clone dotfiles ~/.cfg -- --bare
 rm .bashrc .zshrc .config/fish/fish_variables
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout ubuntu
+
+echo "All done!"
