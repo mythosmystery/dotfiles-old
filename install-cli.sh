91dd4b545429ff6fff40baa29c7d8f0dc44111c8
Hunter Barton <axiysfire@gmail.com>
2022-02-01 23:38:42 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -2,13 +2,8 @@ sudo apt -y update
 sudo apt -y upgrade
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
-sudo apt -y install zsh tmux gcc neovim
+sudo apt -y install zsh tmux gcc neovim vifm fish neofetch
 cargo install exa
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt -y update
-sudo apt -y install gh
-gh auth login
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -18,6 +13,12 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt -y update
+sudo apt -y install gh
+gh auth login
 gh repo clone dotfiles ~/.cfg -- --bare
 rm .bashrc .zshrc
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout ubuntu
