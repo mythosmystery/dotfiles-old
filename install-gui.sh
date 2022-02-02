3cebcf74e046f9a4f2e91dfb59f5620338c1d207
Hunter Barton <axiysfire@gmail.com>
2022-02-02 15:22:32 -0600

    Update install-gui.sh

--- install-gui.sh
+++ install-gui.sh
@@ -1,12 +1,21 @@
+echo "update software"
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y wget
+
+echo "install chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+echo "install vscode"
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] \
 https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update -y
 sudo apt install -y code
+
+echo "install steam"
 sudo apt install -y steam
+
+echo "All done!"
