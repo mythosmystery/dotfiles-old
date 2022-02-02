45c15f1b0a2958b5763d3cdac2533b6e9c881a66
Hunter Barton <axiysfire@gmail.com>
2022-02-01 22:06:21 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -17,4 +17,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 gh repo clone dotfiles ~/.cfg -- --bare
+rm .bashrc .zshrc
 /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout ubuntu
