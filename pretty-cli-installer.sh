33102a891326bd57e4e851d91c8e1759bbbab100
Hunter Barton <axiysfire@gmail.com>
2022-02-04 06:44:11 -0600

    Update pretty-cli-installer.sh

--- pretty-cli-installer.sh
+++ pretty-cli-installer.sh
@@ -19,6 +19,7 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://github.com/gpakosz/.tmux.git
