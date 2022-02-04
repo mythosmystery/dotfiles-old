a01c348bde9e4dda85c67d41be9d65c946dccf65
Hunter Barton <axiysfire@gmail.com>
2022-02-03 23:01:28 -0600

    Update install-cli.sh

--- install-cli.sh
+++ install-cli.sh
@@ -1,11 +1,11 @@
 echo "update pacman and install pikaur"
 sudo pacman -Syyu --noconfirm gcc base-devel
-git clone https://aur.archlinux.org/paru.git $HOME/paru
-cd $HOME/paru
+git clone https://aur.archlinux.org/pikaur.git $HOME/pikaur
+cd $HOME/pikaur
 makepkg -si
 cd $HOME
 sudo pacman -S --noconfirm zsh fish exa htop neofetch wget man-db tmux pacman-contrib xclip lxappearance-gtk3 github-cli alacritty
-paru -S --noconfirm google-chrome visual-studio-code-bin appimagelauncher vim-plug zsh-autosuggestions zsh-syntax-highlighting-git zsh-theme-powerlevel10k starship palenight-gtk-theme
+pikaur -S --noconfirm google-chrome visual-studio-code-bin appimagelauncher vim-plug zsh-autosuggestions zsh-syntax-highlighting-git zsh-theme-powerlevel10k starship palenight-gtk-theme
 gh auth login
 gh repo clone dotfiles ~/.cfg -- --bare
 rm .zshrc .bashrc
