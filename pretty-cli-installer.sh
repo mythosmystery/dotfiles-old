d09c53c3b402148c7a5948f2a979f8c78d110830
Florida <axiysfire@gmail.com>
2022-02-05 13:28:42 +0000

    updated fish

--- pretty-cli-installer.sh
+++ pretty-cli-installer.sh
@@ -14,6 +14,7 @@ echo "install exa"
 cargo install exa
 
 echo "install zsh plugins"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
